17fc5f7649c9a288e3b75b6ca11fd9c0679dd5c7
lucascresencio <lu.cresencio@gmail.com>
2025-01-16 09:47:26 -0300

    post mobilizer

--- api/models.py
+++ api/models.py
@@ -18,3 +18,22 @@ class Maintainer(BaseModel):
     org_id: int = None
     role_id: int = None
     status: str = None
+
+class Mobilizer(BaseModel):
+    email: str = None
+    name: str = None
+    phone: int = None
+    cpf: int = None
+    birthday: str = None
+    zip: str = None
+    street: str = None
+    neighborhood: str = None
+    number: str = None
+    complement: Optional[str] = None
+    city: str = None
+    state: str = None
+    member_since: Optional[str] = None
+    org_id: int = None
+    role_id: int = None
+    status: str = None
+    commission: Optional[float] = None
--- api/registration.py
+++ api/registration.py
@@ -1,10 +1,18 @@
 from db.connection import supabase_connection
 
-from api.models import Maintainer
+from api.models import Maintainer, Mobilizer
 
 from fastapi import APIRouter
 
 router = APIRouter()
+
+# role_id rules:
+# 1 : admin
+# 2 : finance_staff
+# 3 : office_staff
+# 4 : maintainer
+# 5 : mobilizer
+# 6 : volunteer
 
 # create maintainer user
 @router.post("/maintainers")
@@ -34,7 +42,7 @@ def create_maintainer(maintainer: Maintainer):
          "cpf_cnpj": maintainer.cpf,
          "birthday": maintainer.birthday,
          "fk_org": maintainer.org_id,
-         "role_id": maintainer.role_id,
+         "role_id": 4,
          "status": maintainer.status,
          "fk_address": address_id
          }
@@ -42,6 +50,47 @@ def create_maintainer(maintainer: Maintainer):
 
     response = supabase_connection().table("maintainer").insert([{
         "member_since": maintainer.member_since,
+        "fk_user": response_auth.user.id
+    }]).execute()
+
+    return response
+
+# create mobilizer user
+@router.post("/mobilizers")
+def create_maintainer(mobilizer: Mobilizer):
+    # auth user - create account
+    response_auth = supabase_connection().auth.sign_up(
+        {"email": mobilizer.email, "password": "leet123"}
+    )
+
+    response_address = supabase_connection().table("address").insert([{
+        "street": mobilizer.street,
+        "neighborhood": mobilizer.neighborhood,
+        "zip": mobilizer.zip,
+        "complement": mobilizer.complement,
+        "number": mobilizer.number,
+        "city": mobilizer.city,
+        "state": mobilizer.state
+    }]).execute()
+
+    for address in response_address.data:
+        address_id = address["id"]
+
+    response = supabase_connection().table("profile").insert([
+        {"name": mobilizer.name,
+         "fk_user": response_auth.user.id,
+         "phone": mobilizer.phone,
+         "cpf_cnpj": mobilizer.cpf,
+         "birthday": mobilizer.birthday,
+         "fk_org": mobilizer.org_id,
+         "role_id": 5,
+         "status": mobilizer.status,
+         "fk_address": address_id
+         }
+    ]).execute()
+
+    response = supabase_connection().table("mobilizer").insert([{
+        "member_since": mobilizer.member_since,
         "fk_user": response_auth.user.id
     }]).execute()
 
